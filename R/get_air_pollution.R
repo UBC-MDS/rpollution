66208e4be3d2aa4bd2c674aa78327f19f729a7f3
mel-liow <mel.liow.94@gmail.com>
2022-01-22 00:30:41 -0800

    feat: add pollution history function

--- R/get_air_pollution.R
+++ R/get_air_pollution.R
@@ -4,8 +4,8 @@
 #' and fetches pollution data for a given location, then transforms
 #' the returned JSON object from the request into a ggplot chart.
 #'
-#' @param lat Latitude for a given location as a float/integer
-#' @param lon Longitude for a given location as a float/integer
+#' @param lat Latitude for a given location as a double
+#' @param lon Longitude for a given location as a double
 #' @param api_key The API key for OpenWeather as a string
 #' @param fig_title The title of the returned ggplot as a string
 #'
