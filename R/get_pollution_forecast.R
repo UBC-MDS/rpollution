c4cf1b81b42ba6b12b5d8e3a403b55f0c2519fee
mel-liow <mel.liow.94@gmail.com>
2022-01-25 07:45:39 -0800

    feat: add mock data to json

--- R/get_pollution_forecast.R
+++ R/get_pollution_forecast.R
@@ -103,6 +103,8 @@ get_pollution_forecast <- function(lat, lon, api_key) {
     }
   }
   else{
+    print('HERE????')
+    print(api_error)
     "An error occurred fetching data from the API"
   }
 }
